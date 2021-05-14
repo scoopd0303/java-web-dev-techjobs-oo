6b9dcc0d4a761bf4133ec518c2d0a937fcb9eec7
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2021-05-14 13:20:13 -0500

    finished steps 1 and 2 of the assignment

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -3,8 +3,10 @@ package org.launchcode.techjobs_oo;
 import java.util.Objects;
 
 public class CoreCompetency {
+
     private int id;
     private static int nextId = 1;
+
     private String value;
 
     public CoreCompetency() {
@@ -39,4 +41,9 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+
+    public int getId() { return id; }
+
+    public String getValue() { return value; }
+    public void setValue(String value) { this.value = value; }
 }
